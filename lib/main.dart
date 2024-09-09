6a0a39c425bd83e5e8f0a4baaca3cd3ea0efb254
yasithabhanuka <yasithabhanukac@gmail.com>
2024-09-09 01:00:46 -0700

    Create the row widget and column widget both for make designing the bottom container

--- lib/main.dart
+++ lib/main.dart
@@ -22,37 +22,70 @@ class MyApp extends StatelessWidget {
             backgroundColor: Colors.blue[900],
           ),
           body: Container(
-            padding: EdgeInsets.all(30),
-            height: double.infinity,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: Colors.blue[100],
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text("Welcome to Hello Futter App!"),
-                Column(
+              padding: EdgeInsets.all(30),
+              height: double.infinity,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.blue[100],
+              ),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Text("Image"),
-                    SizedBox(
-                      height: 15,
+                    Text(
+                      "Welcome to Hello Futter App!",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.italic),
                     ),
-                    Text("This app is developed by Yasitha!"),
-                  ],
-                ),
-                Container(
-                    padding: EdgeInsets.all(10),
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.circular(20),
+                    Column(
+                      children: [
+                        Text("Image"),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Text("This app is developed by Yasitha!",
+                            style: TextStyle(fontWeight: FontWeight.bold)),
+                      ],
                     ),
-                    child: Text("3 children")),
-              ],
-            ),
-          )),
+                    Container(
+                        padding: EdgeInsets.all(20),
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Row(
+                          children: [
+                            Text(
+                              "Image",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            SizedBox(
+                              width: 15,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Yasitha Bhanuka",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  "Flutter Developer",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                Text(
+                                  "www.yasitha.com",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ],
+                            ),
+                          ],
+                        )),
+                  ]))),
     );
   }
 }
